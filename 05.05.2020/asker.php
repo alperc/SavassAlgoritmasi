b4b1881210b45701a22a687c9a6f54fe58ec22fe
Alper <alperc58@gmail.com>
2020-05-06 19:34:33 +0300

    Geliştirme
    Askerleri hız değerine göre sıralama işlevini eklendi.
    Toplam güç ve can verileri hesaplandı.

--- 05.05.2020/asker.php
+++ 05.05.2020/asker.php
@@ -1,32 +1,170 @@
 <pre>
-
 <?php
 
 session_start();
 
 ob_start();
 
-$askerler = new askerler();
+$ordular = new ordular();
 
-	class askerler
-	
+	class ordular
 	/*
-		# Bu sınıf taraflara ait askerlerin verileri yer alacak. 
-		# Bu veriler bir üst olacak olan savaş sistemi içinde çekilip kullanılacak.
+		# Bu sınıfta askerler adı altında ki taraflarin verileri tutulacak.
+		# Amaç ordular hakkında bilgilerin düzenli olarak tutulması. ve savaşa hazırlanması.
 	*/
+
 	{
 
-		private $askerler = [];
+		private $askerler = []; 		# savasta ve ya tatbikatta yer alacak askerleri tutar.
 
+		private $orduaskerTurSayisi;
+
+		private $orduBilgisi = [];
+
+		private $tarafBilgisi = [];
+
+		private $saldiriSirasi; 		# bu degiskende savaştaki saldırı sırası yer alacak.
+
+		private $torduBilgisi;			# toplam ordu güç verisi yazar.
+
+		private $askeryuklemebittiMi; 	# asker yukleme islemi bittiğinde true değerini alacak ve 
+										# ilksaldiracak ve savas sirasını belirleyecek.
+
+		public function tveri()
+		
+		{
+		
+			return $this->ToplamAskerSay;
+		
+		}
+		
 		public function add($hid=null,$asker)
 
 		{
 
-			$hid;
+			$this->askerler[] = $asker; //  $this->askerler[$hid][] = $asker;
 
-			$this->askerler[$hid][] = $asker;
+			if ( !in_array($hid, $this->tarafBilgisi) ) 
+	
+			{
 
+				array_push( $this->tarafBilgisi,$asker[hid]);
+			
+			}
+				
+				array_push( $this->orduBilgisi,array("taraf" =>$asker[hid],"asker_turu" =>$asker[bid],"asker_sayisi" =>$asker[sayisi]) );
+			
 			//$asker->setTakim($this);
+			
+			return $this;	
+
+		}
+
+		public function veri()
+		
+		{
+
+			//print_r($this->orduBilgisi);
+
+			
+
+			$hizlidanyavasa = $this->sirala($this->askerler, array('hiz'=>SORT_DESC)); //kucukten buyuge
+
+			return $hizlidanyavasa;
+
+		}
+
+		public function end()
+
+		{
+
+			$this->askeryuklemebittiMi=true;
+
+			
+
+			foreach ($this->askerler as $key => $value) 
+
+			{
+				
+				for ($tarafid=0; $tarafid < count($this->tarafBilgisi); $tarafid++) 
+
+				{ 
+					
+					if($this->tarafBilgisi[$tarafid] == $value[hid])	
+
+					{
+
+						$this->torduBilgisi[$this->tarafBilgisi[$tarafid]][sayisi] += $value[sayisi];
+						
+						$this->torduBilgisi[$this->tarafBilgisi[$tarafid]][tcan] += $value[tcan];
+						
+						$this->torduBilgisi[$this->tarafBilgisi[$tarafid]][tguc] += $value[tguc];
+
+					}
+				}
+			
+			}
+
+			//print_r($this->torduBilgisi);
+		}
+
+		public function sirala($array, $cols) 
+		
+		{
+	   
+    		$colarr = array();
+    	
+    		foreach ($cols as $col => $order) 
+	
+    		{
+    	    
+    	    		$colarr[$col] = array();
+    	    
+    	    		foreach ($array as $k => $row) 
+    	    
+    	    		{ 
+	
+    	    			$colarr[$col]['_'.$k] = strtolower($row[$col]); 
+	
+    	    		}
+	
+    		}
+	
+    		$eval = 'array_multisort(';
+	
+    		foreach ($cols as $col => $order) 
+	
+    		{
+	
+    	    		$eval .= '$colarr[\''.$col.'\'],'.$order.',';
+	
+    		}
+	
+    		$eval = substr($eval,0,-1).');';
+	
+    		eval($eval);
+	
+    		$ret = array();
+	
+    		foreach ($colarr as $col => $arr) 
+	
+    		{
+	
+    	    		foreach ($arr as $k => $v) 
+	
+    	    		{
+    	        
+    	        		$k = substr($k,1);
+    	        
+    	        		if (!isset($ret[$k])) $ret[$k] = $array[$k];
+    	       
+    	        		$ret[$k][$col] = $array[$k][$col];
+    	    
+    	    		}
+    	
+    		}
+	
+    		return $ret;	
 
 		}
 
@@ -35,11 +173,8 @@ $askerler = new askerler();
 	class asker
 	/*
 	
-	# Bu sınıfta savaşta yer alan askerlerin bilgileri yer alacak örnek (sayisi,seviye,guc,hiz,can,toplamguc,toplamcan,takim vb)
-	# Bu bilgiler bir üstündeki sınıf olan $askerler içinde taraf şartına göre depolanacak. 
+		# Bu sınıfta savaşta yer alan askerlerin bilgileri yer alacak örnek (sayisi,seviye,guc,hiz,can,toplamguc,toplamcan,takim vb)
 	
-	->nitelik($hid,$bid) hesap ve birim bilgilerini veri tabanından alıp değikenleri doldurulduğu fonksiyon.
-
 	*/
 	{
 		
@@ -47,7 +182,7 @@ $askerler = new askerler();
 
 		public $bid; // türünü belirler.
 
-		private $adi; // sistem veritabandan çekecek.
+		private $adi; // sistem vertabandan çekecek.
 
 		public $sayisi;
 
@@ -59,10 +194,15 @@ $askerler = new askerler();
 
 		private $can; // sistem kendi hesaplayacak.
 
+		private $tguc; // sistem kendi hesaplayacak.
+
+		private $tcan; // sistem kendi hesaplayacak.
+
 		private $otoveri; // otomatik veri doldurma  sistemi aktifse (true) verileri sql'den çeker. ona göre hedefler.
 				# bunun kullanılma sebebi savunan ve ya saldıran tarafın hedef ve hız bilgilerini elle girilmesini sağlamaktır.
 		private $hedef; // otomatik veri doldurma sistemi (false) kapalıysa hedefi kendin girersin.
 
+		private $asker = []; # asker verisin array halince çıktısıdır.
 
 		function __construct($bid=null,$hid=null,$sayisi=null,$otoveri=true,$hedef=null,$seviye=null)
 
@@ -108,11 +248,11 @@ $askerler = new askerler();
 			
 			$varsayilan = ($Birim->vbirimcek($bid));
 			
-			$birimseviye = $Birim->birimseviyecek($hid); # veritabanında birim seviyelerin çeker.
-
 			if($this->otoveri==true)
 
 			{
+
+				$birimseviye = $Birim->birimseviyecek($hid); # veritabanında birim seviyelerin çeker.
 
 				$this->seviyesi = $birimseviye[0][$bid];
 
@@ -120,32 +260,110 @@ $askerler = new askerler();
 			
 			}
 				
-			$this->guc = $varsayilan[vb_guc] + floor($birimseviye[0][$bid]*($varsayilan[vb_guc]/5));
+			$this->guc = $varsayilan[vb_guc] + floor($this->seviyesi*($varsayilan[vb_guc]/5));
 			
-			$this->can = $varsayilan[vb_can] + floor($birimseviye[0][$bid]*($varsayilan[vb_can]/5));
+			$this->can = $varsayilan[vb_can] + floor($this->seviyesi*($varsayilan[vb_can]/5));
 			
-			$this->hiz = $varsayilan[vb_hiz] + floor(($birimseviye[0][$bid])*($varsayilan[vb_hiz]/5));
+			$this->hiz = $varsayilan[vb_hiz] + floor(($this->seviyesi)*($varsayilan[vb_hiz]/5));
 			
 			$this->adi = $Birim->birimisim($bid);
+
+			$this->tcan = $this->can * $this->sayisi;
+
+			$this->tguc = $this->guc * $this->sayisi;
+
+			$this->add();
 
 			return;
 
 		}
 
+		public function add()
+
+		{
+
+			$this->asker[] = array(
+
+				"hid" => $this->hid,
+				"bid" => $this->bid,
+				"adi" => $this->adi,
+				"sayisi" => $this->sayisi,
+				"seviyesi" => $this->seviyesi,
+				"guc" => $this->guc,
+				"hiz" => $this->hiz,
+				"can" => $this->can,
+				"tcan" => $this->tcan,
+				"tguc" => $this->tguc,
+				"hedef" => $this->hedef,
+			 );
+
+			//$asker->setTakim($this);
+			return $this;
+
+		}
+
+		public function veri()
+
+		{
+
+
+			return $this->asker[0];
+
+		}
+
 	}
 
-$asker1 = new asker(1,2,333,true); // asker( "birim türü/cinsi","takim id","birim sayısı","veritabanindan verileri cek")
-$asker2 = new asker(2,2,333,true); 
+$asker1 = new asker(1,A,333,true); // A TAKIMI 1 TIPI
+$asker2 = new asker(2,A,333,true); // A TAKIMI 2 TIPI
 
-$asker3 = new asker(1,58,333,false,1,4); // asker( "birim türü/cinsi","takim id","birim sayısı","verileri cekme","hedef","seviye")
-$asker4 = new asker(2,58,333,false,2,4);
+$asker3 = new asker(1,B,333,false,4,44); // B TAKIMI 1 TIPI
+$asker4 = new asker(2,B,333,false,4,44); // B TAKIMI 2 TIPI
 
-$askerler->add($asker1->hid,$asker1); // $askerler-add("askerin ait olduğu taraf","askere ait veriler");
-$askerler->add($asker2->hid,$asker2);
-$askerler->add($asker3->hid,$asker3);
-$askerler->add($asker4->hid,$asker4);
+$ordular 	->add($asker1->hid,$asker1->veri())
+ 			->add($asker2->hid,$asker2->veri())
 
-print_r($askerler);
+			->add($asker3->hid,$asker3->veri())
+			->add($asker4->hid,$asker4->veri())
+			->end();
+
+$veriler = $ordular->veri();
+
+
+
+print_r($ordular );
+
+
+class Savas
+
+{
+	
+	public function baslat()
+	
+	{
+		
+		$taraf = $this->getSaldiriSiraliAskerArr();
+		
+		$canliSayisi = $this->getCanliSayisi();
+		
+		while( $canliSayisi > 1 )
+		
+		{
+			
+			foreach( $taraf as $asker)
+			
+			{
+				
+				$this->saldir( $asker, $this->hedefBelirle($asker) );
+			
+			}
+
+			$canliSayisi = $this->getCanliSayisi();
+
+		}
+
+	}
+
+}
 
 ?>
 </pre>
