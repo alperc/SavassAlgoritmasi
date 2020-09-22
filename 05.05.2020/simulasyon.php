d93eab2a3e8213163626601e98404e64f4b0f076
Alper <alperc58@gmail.com>
2020-09-22 12:09:21 +0300

    Update simulasyon.php

--- 05.05.2020/simulasyon.php
+++ 05.05.2020/simulasyon.php
@@ -1,11 +1,11 @@
 <pre>
 <?php
 
-$asker1 = new asker(1,Alper,333,false,2,50); // (Asker Türü	, Kime Ait 	, Sayısı 	, Sql Durumu 	, Saldıracağı Hedef 	, Seviyesi)
-$asker2 = new asker(2,Alper,333,false,2,50); // (Alper 		, Sipahi 	, 333		, false		, 2(Musellem)		, 50)
+$asker1 = new asker(1,"Alper",333,false,2,50); // (Asker Türü	, Kime Ait 	, Sayısı 	, Sql Durumu 	, Saldıracağı Hedef 	, Seviyesi)
+$asker2 = new asker(2,"Alper",333,false,2,50); // (Alper 		, Sipahi 	, 333		, false		, 2(Musellem)		, 50)
 
-$asker3 = new asker(1,Çağlar,333,false,1,50);
-$asker4 = new asker(2,Çağlar,333,false,1,0);
+$asker3 = new asker(1,"Çağlar",333,false,1,50);
+$asker4 = new asker(2,"Çağlar",333,false,1,0);
 
 $Ordular 	->add($asker1->hid,$asker1->veri())
  		->add($asker2->hid,$asker2->veri())
